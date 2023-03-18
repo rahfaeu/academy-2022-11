5c7825eeaa1d39d307598b4f685c24e76d15e535
Rafael dos Santos <rahfaeu@gmail.com>
2023-03-18 12:48:11 +0000

    Criando tabela categories

---
+++ models/staging/erp_northwind/stg_erp_northwind_categories.sql
@@ -0,0 +1,10 @@
+with
+    source_categories as (
+        select
+            cast(category_id as int) as id_categoria				
+            , cast(category_name as string) as nome_categoria			
+            , cast(description as string) as descricao_categoria		
+        from {{ source('erp', 'categories') }}
+    )
+select *
+from source_categories
